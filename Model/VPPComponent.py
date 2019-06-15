9a29f2c40dfdbebb1b7b5c10d9520616cbcbd48c
Lars Petersen <lars.p@me.com>
2019-06-15 14:30:11 +0200

    Modified VPPComponent to account for observations
    The VPPComponent class now contains a function called „observationsForTimestamp“. This function returns a key-value-paired dictionary with the different observations for the given component (like SoC of an energy storage).

--- Model/VPPComponent.py
+++ Model/VPPComponent.py
@@ -1,11 +1,7 @@
 
-# TODO: Implement valueForTimestamp with reference to the environment object in child classes!
-
 class VPPComponent(object):
     
-    # The parameter timebase determines the resolution of the given data.
-    # Furthermore the parameter environment (VPPEnvironment) is given to provide weather data and further external influences.
-    # To account for different people using a component, a use case (VPPUseCase) can be passed in to improve the simulation.
+    # The parameter timebase determines the resolution of the given data. Furthermore the parameter environment (VPPEnvironment) is given to provide weather data and further external influences. To account for different people using a component, a use case (VPPUseCase) can be passed in to improve the simulation.
     def __init__(self, timebase, environment, userProfile):
     
         # Configure attributes
@@ -18,10 +14,7 @@ class VPPComponent(object):
     
     
     
-    # This function takes a timestamp as the parameter and returns the
-    # corresponding value for that timestamp. A positiv result represents
-    # a load. A negative result represents a generation.
-    # This abstract function needs to be implemented by child classes.
+    # This function takes a timestamp as the parameter and returns the corresponding value for that timestamp. A positiv result represents a load. A negative result represents a generation. This abstract function needs to be implemented by child classes.
     def valueForTimestamp(self, timestamp):
     
         # Raise error since this function needs to be implemented by child classes.
@@ -31,63 +24,18 @@ class VPPComponent(object):
     
     
     
-    # This function is called to prepare the time series for generations and consumptions
-    # that are based on a non controllable data series. An empty array is stored for generation
-    # units that are independent of external influences.
+    # This function takes a timestamp as the parameter and returns a dictionary with key (String) value (Any) pairs. Depending on the type of component, different status parameters of the respective component can be queried. For example, a power store can report its "State of Charge".
+    def observationsForTimestamp(self, timestamp):
+    
+        # Raise error since this function needs to be implemented by child classes.
+        raise NotImplementedError("observationsForTimestamp needs to be implemented by child classes!")
+
+    
+    
+    
+    
+    # This function is called to prepare the time series for generations and consumptions that are based on a non controllable data series. An empty array is stored for generation units that are independent of external influences.
     def prepareTimeSeries(self):
     
         # Set empty array. Override this function if generation or consumption is based on data series.
         self.timeseries = []
-
-    
-
-
-
-    # This function converts the given timebase to a target timebase. This is achieved by calculating
-    # the average. The return value is either None, if an error occured, or an array of averaged values.
-    def averageDataToTimebase(self, targetTimebase):
-
-        # Validate input parameters
-        if targetTimebase > self.timebase:
-
-            # Calculate compression factor
-            compressionFactor = targetTimebase / self.timebase
-            
-            # Calculate number of iterations
-            numberOfIterations = int(len(self.timeseries) / compressionFactor)
-            
-            
-            # Create results array
-            result = []
-            
-            # Iterate through timeseries
-            for i in range(0, numberOfIterations):
-                
-                sum = 0
-                count = 0
-                
-                for j in range(0, targetTimebase):
-                    
-                    # Add to sum
-                    sum += self.timeseries[i * compressionFactor + j]
-                    
-                    # Increase count
-                    count += 1
-                    
-
-                # Validate count
-                if count > 0:
-
-                    # Calculate average
-                    average = sum / count
-                    
-                    # Append to result
-                    result.append(average)
-
-
-            return result
-
-        else:
-
-            # Parameter is invalid
-            return None
