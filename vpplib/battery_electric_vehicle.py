06e9d6ea41bb723aa6f7f8258010feadddaada4f
Pyosch <birk-sascha@web.de>
2019-12-09 16:43:30 +0100

    black reformatting applied

--- vpplib/battery_electric_vehicle.py
+++ vpplib/battery_electric_vehicle.py
@@ -13,15 +13,25 @@ import random
 
 
 class BatteryElectricVehicle(Component):
-
-    def __init__(self, battery_max, battery_min, battery_usage,
-                 charging_power, load_degradation_begin,
-                 charge_efficiency, unit, identifier=None,
-                 environment=None, user_profile=None, cost=None):
+    def __init__(
+        self,
+        battery_max,
+        battery_min,
+        battery_usage,
+        charging_power,
+        load_degradation_begin,
+        charge_efficiency,
+        unit,
+        identifier=None,
+        environment=None,
+        user_profile=None,
+        cost=None,
+    ):
 
         # Call to super class
-        super(BatteryElectricVehicle, self).__init__(unit, environment,
-                                                     user_profile, cost)
+        super(BatteryElectricVehicle, self).__init__(
+            unit, environment, user_profile, cost
+        )
 
         """
         Info
@@ -108,18 +118,22 @@ class BatteryElectricVehicle(Component):
         """
 
         self.timeseries = pd.DataFrame(
-                pd.date_range(start=self.environment.start,
-                              end=self.environment.end,
-                              freq=self.environment.time_freq, name='Time'))
-        self.timeseries['car_charger'] = 0
+            pd.date_range(
+                start=self.environment.start,
+                end=self.environment.end,
+                freq=self.environment.time_freq,
+                name="Time",
+            )
+        )
+        self.timeseries["car_charger"] = 0
         self.timeseries.set_index(self.timeseries.Time, inplace=True)
 
         self.split_time()
         self.set_weekday()
         self.set_at_home()
         self.charge()
-        self.timeseries.set_index('Time', inplace=True, drop=True)
-        self.timeseries['at_home'] = self.at_home
+        self.timeseries.set_index("Time", inplace=True, drop=True)
+        self.timeseries["at_home"] = self.at_home
 
         return self.timeseries
 
@@ -175,8 +189,9 @@ class BatteryElectricVehicle(Component):
         for i, at_home in self.at_home.iterrows():
             if (at_home.item() == 0) & (battery_charge > self.battery_min):
                 # if car is not at home discharge battery with X kW
-                battery_charge = (battery_charge - self.battery_usage
-                                  * (self.environment.timebase/60))
+                battery_charge = battery_charge - self.battery_usage * (
+                    self.environment.timebase / 60
+                )
 
                 if battery_charge < self.battery_min:
                     battery_charge = self.battery_min
@@ -186,25 +201,29 @@ class BatteryElectricVehicle(Component):
                 lst_charger.append(0)
 
             # Function to apply the load_degradation to the load profile
-            elif ((at_home.item() == 1) and
-                  (battery_charge > self.battery_max
-                   * self.load_degradation_begin)):
-                degraded_charging_power = (
-                        self.charging_power
-                        * (1 - (battery_charge
-                                / self.battery_max
-                                - self.load_degradation_begin)
-                            / (1 - self.load_degradation_begin)))
+            elif (at_home.item() == 1) and (
+                battery_charge > self.battery_max * self.load_degradation_begin
+            ):
+                degraded_charging_power = self.charging_power * (
+                    1
+                    - (
+                        battery_charge / self.battery_max
+                        - self.load_degradation_begin
+                    )
+                    / (1 - self.load_degradation_begin)
+                )
 
-                battery_charge = battery_charge + (degraded_charging_power
-                                                   * self.charge_efficiency
-                                                   * (self.environment.timebase
-                                                      / 60))
+                battery_charge = battery_charge + (
+                    degraded_charging_power
+                    * self.charge_efficiency
+                    * (self.environment.timebase / 60)
+                )
                 charger = degraded_charging_power
 
                 if battery_charge > self.battery_max:
-                    charger = self.charging_power - (battery_charge
-                                                     - self.battery_max)
+                    charger = self.charging_power - (
+                        battery_charge - self.battery_max
+                    )
                     battery_charge = self.battery_max
 
                 lst_battery.append(battery_charge)
@@ -213,16 +232,18 @@ class BatteryElectricVehicle(Component):
             # If car is at home, charge with charging power.
             # If timescale is hours charging power results in kWh
             elif (at_home.item() == 1) & (battery_charge < self.battery_max):
-                battery_charge = battery_charge + (self.charging_power
-                                                   * self.charge_efficiency
-                                                   * (self.environment.timebase
-                                                      / 60))
+                battery_charge = battery_charge + (
+                    self.charging_power
+                    * self.charge_efficiency
+                    * (self.environment.timebase / 60)
+                )
                 charger = self.charging_power
 
                 # If battery would be overcharged, charge only with kWh left
                 if battery_charge > self.battery_max:
-                    charger = self.charging_power - (battery_charge
-                                                     - self.battery_max)
+                    charger = self.charging_power - (
+                        battery_charge - self.battery_max
+                    )
                     battery_charge = self.battery_max
 
                 lst_battery.append(battery_charge)
@@ -235,7 +256,7 @@ class BatteryElectricVehicle(Component):
                 lst_battery.append(battery_charge)
                 lst_charger.append(0)
 
-        self.timeseries['car_capacity'] = lst_battery
+        self.timeseries["car_capacity"] = lst_battery
         self.timeseries.car_charger = lst_charger
 
     # In[Separate date and hours]:
@@ -255,8 +276,10 @@ class BatteryElectricVehicle(Component):
 
         """
 
-        df = pd.DataFrame(self.timeseries.index.astype(
-                'str').str.split().tolist(), columns="date hour".split())
+        df = pd.DataFrame(
+            self.timeseries.index.astype("str").str.split().tolist(),
+            columns="date hour".split(),
+        )
         self.date = df.date
         self.date.index = self.timeseries.index
         self.hour = df.hour
@@ -319,32 +342,42 @@ class BatteryElectricVehicle(Component):
 
         """
 
-        if (len(self.user_profile.week_trip_start) == 0 or
-            len(self.user_profile.week_trip_end) == 0 or
-            len(self.user_profile.weekend_trip_start) == 0 or
-            len(self.user_profile.weekend_trip_end) == 0):
+        if (
+            len(self.user_profile.week_trip_start) == 0
+            or len(self.user_profile.week_trip_end) == 0
+            or len(self.user_profile.weekend_trip_start) == 0
+            or len(self.user_profile.weekend_trip_end) == 0
+        ):
             self.user_profile.get_trip_times()
 
         lst = []
 
         for hour, weekday in zip(self.hour, self.weekday):
-            if (hour == '00:00:00') & (weekday < 5):
+            if (hour == "00:00:00") & (weekday < 5):
                 departure = self.user_profile.week_trip_start[
-                        random.randrange(0, (len(
-                                self.user_profile.week_trip_start) - 1), 1)]
+                    random.randrange(
+                        0, (len(self.user_profile.week_trip_start) - 1), 1
+                    )
+                ]
 
                 arrival = self.user_profile.week_trip_end[
-                        random.randrange(0, (len(
-                                self.user_profile.week_trip_end) - 1), 1)]
+                    random.randrange(
+                        0, (len(self.user_profile.week_trip_end) - 1), 1
+                    )
+                ]
 
-            elif (hour == '00:00:00') & (weekday >= 5):
+            elif (hour == "00:00:00") & (weekday >= 5):
                 departure = self.user_profile.weekend_trip_start[
-                        random.randrange(0, (len(
-                                self.user_profile.weekend_trip_start) - 1), 1)]
+                    random.randrange(
+                        0, (len(self.user_profile.weekend_trip_start) - 1), 1
+                    )
+                ]
 
                 arrival = self.user_profile.weekend_trip_end[
-                        random.randrange(0, (len(
-                                self.user_profile.weekend_trip_end) - 1), 1)]
+                    random.randrange(
+                        0, (len(self.user_profile.weekend_trip_end) - 1), 1
+                    )
+                ]
 
             if (hour > arrival) | (hour < departure):
                 lst.append(1)
@@ -377,15 +410,17 @@ class BatteryElectricVehicle(Component):
 
         if type(timestamp) == int:
 
-            return self.timeseries['car_charger'].iloc[timestamp] * self.limit
+            return self.timeseries["car_charger"].iloc[timestamp] * self.limit
 
         elif type(timestamp) == str:
 
-            return self.timeseries['car_charger'].loc[timestamp] * self.limit
+            return self.timeseries["car_charger"].loc[timestamp] * self.limit
 
         else:
-            raise ValueError("timestamp needs to be of type int or string. " +
-                             "Stringformat: YYYY-MM-DD hh:mm:ss")
+            raise ValueError(
+                "timestamp needs to be of type int or string. "
+                + "Stringformat: YYYY-MM-DD hh:mm:ss"
+            )
 
     def observations_for_timestamp(self, timestamp):
 
@@ -404,19 +439,23 @@ class BatteryElectricVehicle(Component):
         if type(timestamp) == int:
 
             car_charger, car_capacity, at_home = self.timeseries.iloc[
-                    timestamp]
+                timestamp
+            ]
 
         elif type(timestamp) == str:
 
-            car_charger, car_capacity, at_home = self.timeseries.loc[
-                    timestamp]
+            car_charger, car_capacity, at_home = self.timeseries.loc[timestamp]
 
         else:
-            raise ValueError("timestamp needs to be of type int or string. " +
-                             "Stringformat: YYYY-MM-DD hh:mm:ss")
+            raise ValueError(
+                "timestamp needs to be of type int or string. "
+                + "Stringformat: YYYY-MM-DD hh:mm:ss"
+            )
 
-        observations = {'car_charger': car_charger,
-                        'car_capacity': car_capacity,
-                        'at_home': at_home}
+        observations = {
+            "car_charger": car_charger,
+            "car_capacity": car_capacity,
+            "at_home": at_home,
+        }
 
         return observations
