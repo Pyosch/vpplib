afb126fd571c97b2c7ae46069e7d476d11a2c85e
Pyosch <birk-sascha@web.de>
2020-04-27 10:48:43 +0200

    Included Operator and extract_results()

--- pf_fzj_profiles.py
+++ pf_fzj_profiles.py
@@ -12,6 +12,8 @@ import pickle
 import pandas as pd
 import random
 
+from vpplib import Operator
+
 start = "2015-03-01 00:00:00"
 end = "2015-03-01 23:45:00"
 
@@ -24,6 +26,8 @@ with open(r'Results/el_dict_2015.pickle', 'rb') as handle:
 
 net = sb.get_simbench_net("1-LV-semiurb4--0-sw")
 print(net)
+
+operator = Operator(virtual_power_plant=None, net=net, target_data=None)
 
 # plot the grid
 pp.plotting.simple_plot(net)
@@ -66,76 +70,78 @@ for idx in pd.date_range(start=start, end=end, freq="15 Min"):
 
 #%%extract results
 
-ext_grid = pd.DataFrame()
-line_loading_percent = pd.DataFrame()
-bus_vm_pu = pd.DataFrame()
-trafo_loading_percent = pd.DataFrame()
-sgen_p_mw = pd.DataFrame()
-load_p_mw = pd.DataFrame()
-storage_p_mw = pd.DataFrame()
+results = operator.extract_results(net_dict)
 
-for idx in net_dict.keys():
+# ext_grid = pd.DataFrame()
+# line_loading_percent = pd.DataFrame()
+# bus_vm_pu = pd.DataFrame()
+# trafo_loading_percent = pd.DataFrame()
+# sgen_p_mw = pd.DataFrame()
+# load_p_mw = pd.DataFrame()
+# storage_p_mw = pd.DataFrame()
 
-    ext_grid = ext_grid.append(
-        net_dict[idx]["res_ext_grid"], ignore_index=True
-    )
-    line_loading_percent[idx] = net_dict[idx][
-        "res_line"
-    ].loading_percent
-    bus_vm_pu[idx] = net_dict[idx]["res_bus"].vm_pu
-    trafo_loading_percent[idx] = net_dict[idx][
-        "res_trafo"
-    ].loading_percent
-    sgen_p_mw[idx] = net_dict[idx]["res_sgen"].p_mw
-    load_p_mw[idx] = net_dict[idx]["res_load"].p_mw
-    storage_p_mw[idx] = net_dict[idx]["res_storage"].p_mw
+# for idx in net_dict.keys():
 
-if len(line_loading_percent.columns) > 0:
-    line_loading_percent = line_loading_percent.T
-    line_loading_percent.rename(
-        net["line"].name, axis="columns", inplace=True
-    )
-    line_loading_percent.index = pd.to_datetime(
-        line_loading_percent.index
-    )
+#     ext_grid = ext_grid.append(
+#         net_dict[idx]["res_ext_grid"], ignore_index=True
+#     )
+#     line_loading_percent[idx] = net_dict[idx][
+#         "res_line"
+#     ].loading_percent
+#     bus_vm_pu[idx] = net_dict[idx]["res_bus"].vm_pu
+#     trafo_loading_percent[idx] = net_dict[idx][
+#         "res_trafo"
+#     ].loading_percent
+#     sgen_p_mw[idx] = net_dict[idx]["res_sgen"].p_mw
+#     load_p_mw[idx] = net_dict[idx]["res_load"].p_mw
+#     storage_p_mw[idx] = net_dict[idx]["res_storage"].p_mw
 
-if len(bus_vm_pu.columns) > 0:
-    bus_vm_pu = bus_vm_pu.T
-    bus_vm_pu.rename(net.bus.name, axis="columns", inplace=True)
-    bus_vm_pu.index = pd.to_datetime(bus_vm_pu.index)
+# if len(line_loading_percent.columns) > 0:
+#     line_loading_percent = line_loading_percent.T
+#     line_loading_percent.rename(
+#         net["line"].name, axis="columns", inplace=True
+#     )
+#     line_loading_percent.index = pd.to_datetime(
+#         line_loading_percent.index
+#     )
 
-trafo_loading_percent = trafo_loading_percent.T
-trafo_loading_percent.index = pd.to_datetime(
-    trafo_loading_percent.index
-)
-if net.trafo.name is not None:
-    trafo_loading_percent.rename(
-        net.trafo.name, axis="columns", inplace=True
-    )
+# if len(bus_vm_pu.columns) > 0:
+#     bus_vm_pu = bus_vm_pu.T
+#     bus_vm_pu.rename(net.bus.name, axis="columns", inplace=True)
+#     bus_vm_pu.index = pd.to_datetime(bus_vm_pu.index)
 
-if len(sgen_p_mw.columns) > 0:
-    sgen_p_mw = sgen_p_mw.T
-    sgen_p_mw.rename(net.sgen.name, axis="columns", inplace=True)
-    sgen_p_mw.index = pd.to_datetime(sgen_p_mw.index)
+# trafo_loading_percent = trafo_loading_percent.T
+# trafo_loading_percent.index = pd.to_datetime(
+#     trafo_loading_percent.index
+# )
+# if net.trafo.name is not None:
+#     trafo_loading_percent.rename(
+#         net.trafo.name, axis="columns", inplace=True
+#     )
 
-if len(load_p_mw.columns) > 0:
-    load_p_mw = load_p_mw.T
-    load_p_mw.rename(net.load.name, axis="columns", inplace=True)
-    load_p_mw.index = pd.to_datetime(load_p_mw.index)
+# if len(sgen_p_mw.columns) > 0:
+#     sgen_p_mw = sgen_p_mw.T
+#     sgen_p_mw.rename(net.sgen.name, axis="columns", inplace=True)
+#     sgen_p_mw.index = pd.to_datetime(sgen_p_mw.index)
 
-if len(storage_p_mw.columns) > 0:
-    storage_p_mw = storage_p_mw.T
-    storage_p_mw.rename(
-        net.storage.name, axis="columns", inplace=True
-    )
-    storage_p_mw.index = pd.to_datetime(storage_p_mw.index)
+# if len(load_p_mw.columns) > 0:
+#     load_p_mw = load_p_mw.T
+#     load_p_mw.rename(net.load.name, axis="columns", inplace=True)
+#     load_p_mw.index = pd.to_datetime(load_p_mw.index)
 
-results = {
-    "ext_grid": ext_grid,
-    "trafo_loading_percent": trafo_loading_percent,
-    "line_loading_percent": line_loading_percent,
-    "bus_vm_pu": bus_vm_pu,
-    "load_p_mw": load_p_mw,
-    "sgen_p_mw": sgen_p_mw,
-    "storage_p_mw": storage_p_mw,
-}
+# if len(storage_p_mw.columns) > 0:
+#     storage_p_mw = storage_p_mw.T
+#     storage_p_mw.rename(
+#         net.storage.name, axis="columns", inplace=True
+#     )
+#     storage_p_mw.index = pd.to_datetime(storage_p_mw.index)
+
+# results = {
+#     "ext_grid": ext_grid,
+#     "trafo_loading_percent": trafo_loading_percent,
+#     "line_loading_percent": line_loading_percent,
+#     "bus_vm_pu": bus_vm_pu,
+#     "load_p_mw": load_p_mw,
+#     "sgen_p_mw": sgen_p_mw,
+#     "storage_p_mw": storage_p_mw,
+# }
