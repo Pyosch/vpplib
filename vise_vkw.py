b0270dc9cb114d1c543a6775fc8e65b16d975ee1
Pyosch <birk-sascha@web.de>
2020-05-28 18:40:13 +0200

    Import preconfigured household_dict, overwrite environment in dict

--- vise_vkw.py
+++ vise_vkw.py
@@ -14,6 +14,7 @@ import pandas as pd
 import random
 import time
 import pickle
+from tqdm import tqdm 
 
 import simbench as sb
 import pandapower as pp
@@ -24,17 +25,17 @@ from vpplib import HeatPump, ThermalEnergyStorage, ElectricalEnergyStorage
 from vpplib import CombinedHeatAndPower
 
 # define virtual power plant
-pv_number = 100
+# pv_number = 100
 wind_number = 10
 bev_number = 50
-hp_number = 50  # always includes thermal energy storage
-ees_number = 80
-chp_number = 0  # always includes thermal energy storage
+# hp_number = 50  # always includes thermal energy storage
+# ees_number = 80
+# chp_number = 0  # always includes thermal energy storage
 
-component_number = pv_number + bev_number + hp_number + chp_number
+# component_number = pv_number + bev_number + hp_number + chp_number
 
 # Simbench Network parameters
-sb_code = "1-MVLV-semiurb-all-0-sw" # "1-MVLV-semiurb-5.220-0-sw" # "1-LV-semiurb4--0-sw"
+sb_code = "1-LV-semiurb4--0-sw" #"1-MVLV-semiurb-all-0-sw" # "1-MVLV-semiurb-5.220-0-sw" # 
 
 # Values for environment
 start = "2015-01-01 00:00:00"
@@ -46,28 +47,7 @@ index_year = pd.date_range(
 )
 index_hours = pd.date_range(start=start, end=end, freq="h", name="time")
 timebase = 15  # for calculations from kW to kWh
-
-# Values for user profile
-identifier = "bus"
-latitude = 50.941357
-longitude = 6.958307
-target_temperature = 60  # °C
-t_0 = 40  # °C
-yearly_thermal_energy_demand = None  # kWh thermal; redifined depending on el baseload
-
-# Values for pv
-module_lib = "SandiaMod"
-# module will be choosen during function call
-inverter_lib = "SandiaInverter"
-# inverter will be choosen during function call
-surface_tilt = 20
-surface_azimuth = 200
-# modules_per_string will be choosen during function call
-# strings_per_inverter will be choosen during function call
-min_module_power = 220
-max_module_power = 250
-#  pv_power will be choosen during function call
-inverter_power_range = 100
+timezone="Europe/Berlin"
 
 # WindTurbine data
 wea_list = [
@@ -92,11 +72,6 @@ density_correction = True
 obstacle_height = 0
 hellman_exp = None
 
-# Values for el storage
-charge_efficiency = 0.98
-discharge_efficiency = 0.98
-# power and capacity will be randomly assigned during component generation
-max_c = 1  # factor between 0.5 and 1.2
 
 # Values for bev
 # power and capacity will be randomly assigned during component generation
@@ -105,75 +80,40 @@ battery_usage = 1
 bev_charge_efficiency = 0.98
 load_degradation_begin = 0.8
 
-# Values for heat pump
-heat_pump_type = "Air"
-heat_sys_temp = 60
-el_power_hp_min = 5
-el_power_hp_max = 11
-th_power_hp = None
-building_type = "DE_HEF33"
-ramp_up_time_hp = 1 / 15  # timesteps
-ramp_down_time_hp = 1 / 15  # timesteps
-min_runtime_hp = 1  # timesteps
-min_stop_time_hp = 2  # timesteps
-
-# Values for Thermal Storage
-hysteresis = 5  # °K
-#  radomly assigned during component generation
-mass_of_storage_min = 400  # kg
-mass_of_storage_max = 800  # kg
-cp = 4.2
-thermal_energy_loss_per_day = 0.13
-
-# Values for chp
-el_power_chp = 6  # kW
-th_power_chp = 10  # kW
-overall_efficiency = 0.8
-ramp_up_time = 1 / 15  # timesteps
-ramp_down_time = 1 / 15  # timesteps
-min_runtime_chp = 1  # timesteps
-min_stop_time_chp = 2  # timesteps
-
 #%% load dicts with electrical and thermal profiles
-
-with open(r'Results/thermal_dict.pickle', 'rb') as handle:
-    thermal_dict = pickle.load(handle)
-
-with open(r'Results/el_dict_2015.pickle', 'rb') as handle:
-    el_dict = pickle.load(handle)
-
-# load COMODO results
-df_tech_input = pd.read_excel(
-    r"./input/input_vise/COMODO/Input_Tech_COMODO_v1.01.xlsx",
-    decimal=",",
-    index_col="tech")
-
-df_installed_cap = pd.read_excel(
-    r"./input/input_vise/COMODO/comodo_results_instaCap.xlsx",
-    decimal=",",
-    sheet_name="out_INSTCAP",
-    index_col=[0,1])
-
-# Year to pick data from COMODO installed capacity
-observation_year = [2025, 2030, 2035, 2040]
+with open(r'Results/20200528_up_dummy_profiles.pickle', 'rb') as handle:
+    household_dict = pickle.load(handle)
 
 print(time.asctime( time.localtime(time.time()) ))
 print("Loaded input\n")
 # %% environment
 
-environment = Environment(
-    timebase=timebase,
-    timezone="Europe/Berlin",
-    start=start,
-    end=end,
-    year=year,
-    time_freq=time_freq,
-)
+# environment = Environment(
+#     timebase=timebase,
+#     timezone="Europe/Berlin",
+#     start=start,
+#     end=end,
+#     year=year,
+#     time_freq=time_freq,
+# )
+#%% Adjust the values of the environments in the household_dict with the
+#   current values. Since the environment is stored in one place
+#   (eg. 0x158c9b24c08) and all components have the same, it only has to be
+#   changed once!
 
-environment.get_mean_temp_days()
-environment.get_mean_temp_hours()
-environment.get_pv_data()
-environment.get_wind_data()
+household_dict[list(household_dict.keys())[0]].environment.start = start
+household_dict[list(household_dict.keys())[0]].environment.end = end
+household_dict[list(household_dict.keys())[0]].environment.timebase = timebase
+household_dict[list(household_dict.keys())[0]].environment.timezone = timezone
+household_dict[list(household_dict.keys())[0]].environment.year = year
+household_dict[list(household_dict.keys())[0]].environment.time_freq = time_freq
+
+# get data for timeseries calculations
+household_dict[list(household_dict.keys())[0]].environment.get_mean_temp_days()
+household_dict[list(household_dict.keys())[0]].environment.get_mean_temp_hours()
+household_dict[list(household_dict.keys())[0]].environment.get_pv_data()
+household_dict[list(household_dict.keys())[0]].environment.get_wind_data()
+
 
 # %% virtual power plant
 
@@ -205,146 +145,72 @@ for index in net.load.index:
 print(time.asctime(time.localtime(time.time())))
 print("Initialized environment, vpp and net\n")
 
-# %% generate user profiles based on grid buses for lv
 
-lv_buses = []
-for bus in net.bus.name:
-    if "LV" in bus:
-        lv_buses.append(bus)
-
-household_dict = dict()
-for house in df_installed_cap.index.get_level_values(0).unique():
-    for y in observation_year:
-        # Create UserProfile for every buildug type
-        user_profile = UserProfile(
-            identifier=(house+"_"+str(y)),
-            latitude=latitude,
-            longitude=longitude,
-            thermal_energy_demand_yearly=None,
-            building_type=None,
-            comfort_factor=None,
-            t_0=t_0,
-            daily_vehicle_usage=None,
-            week_trip_start=[],
-            week_trip_end=[],
-            weekend_trip_start=[],
-            weekend_trip_end=[],
-        )
-        # Include COMODO results
-        user_profile.pv_kwp = df_installed_cap.loc[house].loc[y].PV
-        user_profile.chp_kw_th = df_installed_cap.loc[house].loc[y].CHP_Otto
-        user_profile.chp_el_eff = df_tech_input.loc["CHP_Otto"].efficiency_el
-        user_profile.chp_th_eff = df_tech_input.loc["CHP_Otto"].efficiency_th
-        user_profile.chp_kw_el = ((df_tech_input.loc["CHP_Otto"].efficiency_el
-                                  /df_tech_input.loc["CHP_Otto"].efficiency_th)
-                                  *user_profile.chp_kw_th)
-        user_profile.tes = df_installed_cap.loc[house].loc[y].Th_Stor_water_heat
-        user_profile.heating_rod = df_installed_cap.loc[house].loc[y].SimplePTH
-        user_profile.ees = df_installed_cap.loc[house].loc[y].Batt
-
-        # Include fzj profiles
-        user_profile.el_profile = random.sample(el_dict.keys(), 1)[0]
-
-        if "HH1a" in user_profile.el_profile:
-            if "Winter" in user_profile.el_profile:
-                user_profile.th_profile = 'HH1a_vacationWinter'
-
-            if "Summer" in user_profile.el_profile:
-                user_profile.th_profile = 'HH1a_vacationSummer'
-
-        elif "HH1b" in user_profile.el_profile:
-            if "Dec" in user_profile.el_profile:
-                user_profile.th_profile = 'HH1b_vacationWinter'
-
-            if "Summer" in user_profile.el_profile:
-                user_profile.th_profile = 'HH1b_vacationSummer'
-    
-        elif "HH2a" in user_profile.el_profile:
-            if "Winter" in user_profile.el_profile:
-                user_profile.th_profile = 'HH2a_vacationWinter'
-    
-            if "Summer" in user_profile.el_profile:
-                user_profile.th_profile = 'HH2a_vacationSummer'
-
-        elif "HH2b" in user_profile.el_profile:
-            if "Winter" in user_profile.el_profile:
-                user_profile.th_profile = 'HH2b_vacationWinter'
-    
-            if "Summer" in user_profile.el_profile:
-                user_profile.th_profile = 'HH2b_vacationSummer'
-    
-        else:
-            print("Profile of", user_profile.identifier, " not propperly assigned!")
-
-        user_profile.electric_energy_demand = el_dict[user_profile.el_profile].loc[start:end]
-        user_profile.thermal_energy_demand = thermal_dict[user_profile.th_profile].loc[start:end]
-
-        household_dict[user_profile.identifier] = user_profile
-
-# Assign user_profiles to buses
+#%% Assign user_profiles to buses
 up_dict = dict()
 for bus in net.bus.name:
     if "LV" in bus:
         house = random.sample(household_dict.keys(), 1)[0]
         up_dict[bus+'_'+house] = household_dict[house]
+        up_dict[bus+'_'+house].bus = bus
 
 
 # %% generate user profiles based on grid buses for mv
 
-if wind_number > 0:
-    mv_buses = []
+# if wind_number > 0:
+#     mv_buses = []
 
-    for bus in net.bus.name:
-        if "MV" in bus:
-            mv_buses.append(bus)
+#     for bus in net.bus.name:
+#         if "MV" in bus:
+#             mv_buses.append(bus)
 
-count = 0
-up_with_wind = []
-while count < wind_number:
+# count = 0
+# up_with_wind = []
+# while count < wind_number:
 
-    simbus = random.sample(mv_buses, 1)[0]
-    vpp.buses_with_wind.append(simbus)
+#     simbus = random.sample(mv_buses, 1)[0]
+#     vpp.buses_with_wind.append(simbus)
 
-    user_profile = UserProfile(
-        identifier=simbus,
-        latitude=latitude,
-        longitude=longitude,
-        thermal_energy_demand_yearly=yearly_thermal_energy_demand,
-        building_type=building_type,
-        comfort_factor=None,
-        t_0=t_0,
-        daily_vehicle_usage=None,
-        week_trip_start=[],
-        week_trip_end=[],
-        weekend_trip_start=[],
-        weekend_trip_end=[],
-    )
+#     user_profile = UserProfile(
+#         identifier=simbus,
+#         latitude=latitude,
+#         longitude=longitude,
+#         thermal_energy_demand_yearly=yearly_thermal_energy_demand,
+#         building_type=building_type,
+#         comfort_factor=None,
+#         t_0=t_0,
+#         daily_vehicle_usage=None,
+#         week_trip_start=[],
+#         week_trip_end=[],
+#         weekend_trip_start=[],
+#         weekend_trip_end=[],
+#     )
 
-    #TODO: MAYBE USE FOR aggregated MV loads
-    # Uncomment if loadprofile in user_profile is needed
-    # Keep in mind to include check for loadprofile when choosing "simbus"
-    # like done for lv_buses.
-    #
-    # user_profile.baseload = pd.DataFrame(
-    #     profiles['load', 'p_mw'][
-    #         net.load[net.load.bus == net.bus[
-    #             net.bus.name == simbus].index.item()].iloc[0].name
-    #         ].loc[start:end]
-    #     * 1000)
-    # # thermal energy demand equals two times the electrical energy demand:
-    # user_profile.thermal_energy_demand_yearly = (user_profile.baseload.sum()
-    #                                              / 2).item()  # /4 *2= /2
-    # user_profile.get_thermal_energy_demand()
+#     #TODO: MAYBE USE FOR aggregated MV loads
+#     # Uncomment if loadprofile in user_profile is needed
+#     # Keep in mind to include check for loadprofile when choosing "simbus"
+#     # like done for lv_buses.
+#     #
+#     # user_profile.baseload = pd.DataFrame(
+#     #     profiles['load', 'p_mw'][
+#     #         net.load[net.load.bus == net.bus[
+#     #             net.bus.name == simbus].index.item()].iloc[0].name
+#     #         ].loc[start:end]
+#     #     * 1000)
+#     # # thermal energy demand equals two times the electrical energy demand:
+#     # user_profile.thermal_energy_demand_yearly = (user_profile.baseload.sum()
+#     #                                              / 2).item()  # /4 *2= /2
+#     # user_profile.get_thermal_energy_demand()
 
-    up_with_wind.append(user_profile.identifier)
+#     up_with_wind.append(user_profile.identifier)
 
-    up_dict[user_profile.identifier] = user_profile
-    count += 1
+#     up_dict[user_profile.identifier] = user_profile
+#     count += 1
 
-# create a list of all user profiles and shuffle that list to obtain a random
-# assignment of components to the bus
-up_list = list(up_dict.keys())
-random.shuffle(up_list)
+# # create a list of all user profiles and shuffle that list to obtain a random
+# # assignment of components to the bus
+# up_list = list(up_dict.keys())
+# random.shuffle(up_list)
 
 print(time.asctime(time.localtime(time.time())))
 print("Generated user_profiles\n")
@@ -373,6 +239,63 @@ print("Generated user_profiles\n")
 # #storage_amount = int(round((len(buses_with_pv) * (storage_percentage/100)), 0))
 # vpp.buses_with_ees = random.sample(vpp.buses_with_pv, ees_number)
 
+#%% generate components based on user_profiles
+
+print("generate components based on user_profiles:")
+
+for up in tqdm(up_dict.keys()):
+
+    if up_dict[up].pv_kwp:
+
+        surface_tilt = random.randrange(start=20, stop=40, step=5)
+        surface_azimuth = random.randrange(start=160, stop=220, step=10)
+
+        new_component = Photovoltaic(
+        unit="kW",
+        identifier=(up_dict[up].bus + "_pv"),
+        environment=environment,
+        user_profile=up_dict[up],
+        module_lib=module_lib,
+        module=None,
+        inverter_lib=inverter_lib,
+        inverter=None,
+        surface_tilt=surface_tilt,
+        surface_azimuth=surface_azimuth,
+        modules_per_string=None,
+        strings_per_inverter=None,
+        )
+
+        new_component.pick_pvsystem(min_module_power,
+                                    max_module_power,
+                                    up_dict[up].pv_kwp,
+                                    inverter_power_range)
+
+        new_component.prepare_time_series()
+
+        # TODO
+        # Somehow in some pvlib timeseries the inverter losses during night hours
+        # are not complete. Once we find out how to solve this problem we can
+        # delete this part:
+        if new_component.timeseries.isnull().values.any():
+                new_component.timeseries.fillna(
+                    value=new_component.timeseries.min(),
+                    inplace=True)
+
+        vpp.add_component(new_component)
+
+        # Create element in pandapower network
+        pp.create_sgen(
+        net,
+        bus=net.bus[net.bus.name == up_dict[up].bus].index[0],
+        p_mw=(
+            vpp.components[up_dict[up].bus + "_pv"].module.Impo
+            * vpp.components[up_dict[up].bus + "_pv"].module.Vmpo
+            / 1000000
+        ),
+        q_mvar = 0,
+        name=(up_dict[up].identifier + "_pv"),
+        type="pv",
+        )
 
 # %% generate pv
 
