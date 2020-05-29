60540f6f7c4becd370af4bbd27af5daebdca944e
Pyosch <birk-sascha@web.de>
2020-05-29 14:25:06 +0200

    Added export of timeseries and component values to sql, saved vpp and net objects to pickle

--- vise_vkw.py
+++ vise_vkw.py
@@ -2,10 +2,10 @@
 """
 Created on Tue Jan 28 15:35:09 2020
 
-Generate a virtual power plant based on predefined numbers of buses and
-components.
+Generate a virtual power plant based on predefined user_profiles
 
-Export timeseries and component values to csv-files at the end
+Export timeseries and component values to sql at the end
+Save vpp and corresponding grid to pickle
 
 @author: pyosch
 """
@@ -20,20 +20,14 @@ import copy
 import simbench as sb
 import pandapower as pp
 
-from vpplib import VirtualPowerPlant, UserProfile, Environment, Operator
+from vpplib import VirtualPowerPlant  #, Operator, UserProfile, Environment,
 from vpplib import Photovoltaic, WindPower, BatteryElectricVehicle
 from vpplib import HeatPump, ThermalEnergyStorage, ElectricalEnergyStorage
 from vpplib import CombinedHeatAndPower
 
 # define virtual power plant
-# pv_number = 100
-wind_number = 10
-bev_number = 50
-# hp_number = 50  # always includes thermal energy storage
-# ees_number = 80
-# chp_number = 0  # always includes thermal energy storage
-
-# component_number = pv_number + bev_number + hp_number + chp_number
+wind_number = 0
+bev_number = 0
 
 # Simbench Network parameters
 sb_code = "1-LV-semiurb4--0-sw" #"1-MVLV-semiurb-all-0-sw" # "1-MVLV-semiurb-5.220-0-sw" # 
@@ -163,6 +157,7 @@ for bus in tqdm(net.bus.name):
             up_dict[up_id].pv_system.identifier = up_id+'_pv'
 
             vpp.add_component(up_dict[up_id].pv_system)
+            vpp.components[up_dict[up_id].pv_system.identifier].timeseries.columns = [vpp.components[up_dict[up_id].pv_system.identifier].identifier]
 
             pp.create_sgen(
                 net,
@@ -250,7 +245,8 @@ for bus in tqdm(net.bus.name):
                 type="bev",
             )
 
-
+print(time.asctime(time.localtime(time.time())))
+print("Assigned user profiles to vpp and net\n")
 # %% generate user profiles based on grid buses for mv
 
 # if wind_number > 0:
@@ -308,95 +304,77 @@ for bus in tqdm(net.bus.name):
 # up_list = list(up_dict.keys())
 # random.shuffle(up_list)
 
-print(time.asctime(time.localtime(time.time())))
-print("Generated user_profiles\n")
+# print(time.asctime(time.localtime(time.time())))
+# print("Generated user_profiles\n")
 
 
 # %% generate wea
 
-for bus in vpp.buses_with_wind:
+# for bus in vpp.buses_with_wind:
 
-    new_component = WindPower(
-    unit="kW",
-    identifier=(bus + "_wea"),
-    environment=environment,
-    user_profile=None,
-    turbine_type=wea_list[random.randint(0, (len(wea_list) -1))],
-    hub_height=hub_height,
-    rotor_diameter=rotor_diameter,
-    fetch_curve=fetch_curve,
-    data_source=data_source,
-    wind_speed_model=wind_speed_model,
-    density_model=density_model,
-    temperature_model=temperature_model,
-    power_output_model=power_output_model,
-    density_correction=density_correction,
-    obstacle_height=obstacle_height,
-    hellman_exp=hellman_exp,
-    )
-    new_component.prepare_time_series()
-    vpp.add_component(new_component)
+#     new_component = WindPower(
+#     unit="kW",
+#     identifier=(bus + "_wea"),
+#     environment=environment,
+#     user_profile=None,
+#     turbine_type=wea_list[random.randint(0, (len(wea_list) -1))],
+#     hub_height=hub_height,
+#     rotor_diameter=rotor_diameter,
+#     fetch_curve=fetch_curve,
+#     data_source=data_source,
+#     wind_speed_model=wind_speed_model,
+#     density_model=density_model,
+#     temperature_model=temperature_model,
+#     power_output_model=power_output_model,
+#     density_correction=density_correction,
+#     obstacle_height=obstacle_height,
+#     hellman_exp=hellman_exp,
+#     )
+#     new_component.prepare_time_series()
+#     vpp.add_component(new_component)
 
 # %% generate bev
 
-for bus in vpp.buses_with_bev:
+# for bus in vpp.buses_with_bev:
 
-    new_component = BatteryElectricVehicle(
-    unit="kW",
-    identifier=(bus + "_bev"),
-    battery_max=random.sample([50, 60, 17.6, 64, 33.5, 38.3,75, 20, 27.2, 6.1]
-                              , 1)[0],
-    battery_min=battery_min,
-    battery_usage=battery_usage,
-    charging_power=random.sample([3.6, 11, 22], 1)[0],
-    charge_efficiency=bev_charge_efficiency,
-    environment=environment,
-    user_profile=up_dict[bus],
-    load_degradation_begin=load_degradation_begin,
-    )
+#     new_component = BatteryElectricVehicle(
+#     unit="kW",
+#     identifier=(bus + "_bev"),
+#     battery_max=random.sample([50, 60, 17.6, 64, 33.5, 38.3,75, 20, 27.2, 6.1]
+#                               , 1)[0],
+#     battery_min=battery_min,
+#     battery_usage=battery_usage,
+#     charging_power=random.sample([3.6, 11, 22], 1)[0],
+#     charge_efficiency=bev_charge_efficiency,
+#     environment=environment,
+#     user_profile=up_dict[bus],
+#     load_degradation_begin=load_degradation_begin,
+#     )
 
-    new_component.prepare_time_series()
-    vpp.add_component(new_component)
+#     new_component.prepare_time_series()
+#     vpp.add_component(new_component)
 
 
 
-print(time.asctime(time.localtime(time.time())))
-print("Generated components in vpp\n")
+# print(time.asctime(time.localtime(time.time())))
+# print("Generated components in vpp\n")
 
-# %% initialize operator
+# %% Export vpp to sql
 
-operator = Operator(virtual_power_plant=vpp,
-                    net=net,
-                    target_data=None,
-                    environment=environment)
+savety_timestamp = time.strftime("%Y%m%d-%H%M%S",time.localtime())
 
-print(time.asctime(time.localtime(time.time())))
-print("Initialized Operator\n")
-
-# %% timeseries are in kW, pandapower needs MW
-for component in vpp.components.keys():
-    # el energy storage does not have a timeseries yet
-    if "_ees" not in component:
-        vpp.components[component].timeseries /= 1000
-
-# %% run base_scenario without operation strategies
-net_dict = operator.run_vise_scenario(el_dict)
+vpp.export_components_to_sql(savety_timestamp+"_vpp_export"))
 
 print(time.asctime(time.localtime(time.time())))
-print("Finished run_simbench_scenario()\n")
-# %% extract results from powerflow
+print("Exported component values and timeseries to sql\n")
+#%% Save vpp and net to pickle for later powerflow analysis
 
-results = operator.extract_results(net_dict)
-single_result = operator.extract_single_result(
-    net_dict, res="ext_grid", value="p_mw"
-)
+                    
+with open((r"./Results/"+savety_timestamp+"_vpp_export"+".pickle"),"wb") as handle:
+    pickle.dump(vpp, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+with open((r"./Results/"+savety_timestamp+"_net_export"+".pickle"),"wb") as handle:
+    pickle.dump(net, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 print(time.asctime(time.localtime(time.time())))
-print("Exported results\n")
-# %% plot results of powerflow and storage values
-
-# single_result.plot(
-#     figsize=(16, 9), title="ext_grid from single_result function"
-# )
-# operator.plot_results(results, legend=False)
-# operator.plot_storages()
+print("Saved vpp and net to pickle\n")
