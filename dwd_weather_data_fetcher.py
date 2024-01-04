fb9b5746c9c81ca00d88e8272298d2b52bf367d2
jnscns <jonas.qu@hotmail.de>
2024-01-04 19:02:08 +0100

    latest version

--- dwd_weather_data_fetcher.py
+++ dwd_weather_data_fetcher.py
@@ -11,11 +11,11 @@ Change your settings here
 """
 path_output_main = dir_path + "/dwd_results/"    
 "hourly or daily"
-frequency = 'daily'
-resolution = '60 min'
-distance = 100
-surpress_output_globally = False
-force_end_time = True
+frequency   = 'daily'
+resolution  = '60 min'
+distance    = 100
+surpress_output_globally  = False
+force_end_time            = True
 min_quality_per_parameter = 10
 run_time_hours = 0 #should be max 238 hours. If run_time_hours = 0 the script will run in infinite loop
 dict_locations = {
@@ -32,17 +32,18 @@ for key in dict_locations:
         os.mkdir(dir_path_key)
          
 #Get actual time from dwd server (UTC)
-time_start = Environment().get_time_from_dwd().replace(tzinfo=None)
-forecast_end_time = time_start + datetime.timedelta(hours = run_time_hours + 2)
-plot_next_run_flag = False
+time_start          = Environment().get_time_from_dwd().replace(tzinfo=None)
+obs_start_time      = time_start
+forecast_end_time   = time_start + datetime.timedelta(hours = run_time_hours + 2)
+plot_next_run_flag  = False
 
 if frequency == 'daily':
     #Set shedule time for daily run. First run triggered by run_get_dwd_data() so the first shedule run is time_now+23h58min
-    shedule_time = datetime.datetime.now() + datetime.timedelta(hours=23, minutes = 58)
+    shedule_time     = datetime.datetime.now() + datetime.timedelta(hours=23, minutes = 58)
     str_shedule_time = str(shedule_time.hour).zfill(2) + ':00'
 elif frequency == 'hourly':
-    shedule_time = datetime.datetime.now() + datetime.timedelta(minutes = 2)
-    str_shedule_time =':' + str(shedule_time.minute).zfill(2)
+    shedule_time     = datetime.datetime.now() + datetime.timedelta(minutes = 2)
+    str_shedule_time = ':' + str(shedule_time.minute).zfill(2)
 else:
     raise Exception("unknown frequency")
 
@@ -56,28 +57,27 @@ def create_csv(meta_1,data_1,meta_2,data_2,out_path):
     """
     
     meta_1_sorted = pd.DataFrame(index = meta_1.to_dict(orient='dict').keys(), data = meta_1.to_dict(orient='dict').values())
-    concat_df_1 = pd.concat([meta_1_sorted, data_1], axis=1)
+    concat_df_1   = pd.concat([meta_1_sorted, data_1], axis=1)
     
     meta_2_sorted = pd.DataFrame(index = meta_2.to_dict(orient='dict').keys(), data = meta_2.to_dict(orient='dict').values())
-    concat_df_2 = pd.concat([meta_2_sorted, data_2], axis=1)
+    concat_df_2   = pd.concat([meta_2_sorted, data_2], axis=1)
     
     out_df = pd.concat([concat_df_1,concat_df_2], axis = 1)
-    
     out_df.to_csv(out_path, sep = ';')
 
 def run_get_dwd_data(test_run = False):
     global forecast_end_time
     global plot_next_run_flag
+    global obs_start_time
     if test_run:
         print("Start test run")
     time_now_dwd = Environment().get_time_from_dwd().replace(tzinfo=None)
     print(time_now_dwd)
     if run_time_hours == 0:
         forecast_end_time = time_now_dwd.replace(minute=0,second=0) + datetime.timedelta(hours = 240)
-    if time_now_dwd > time_start + datetime.timedelta(hours = 240) and run_time_hours == 0:
-        obs_start_time = time_now_dwd - datetime.timedelta(hours = 240)
-    else:
-        obs_start_time = time_start
+        if time_now_dwd > time_start + datetime.timedelta(hours = 240):
+            obs_start_time = time_now_dwd - datetime.timedelta(hours = 240)
+        
         
     for location in dict_locations:
         #print("Query weather data for " + str(location))
@@ -91,7 +91,7 @@ def run_get_dwd_data(test_run = False):
                     end=str(time_now_dwd),
                     time_freq=resolution,
                     surpress_output_globally=surpress_output_globally, 
-                    force_end_time= force_end_time)
+                    force_end_time=force_end_time)
                 print("Query Observation data for " + str(location) + " lat: " + str(latitude) + " lon: " + str(longitude))
                 print("Start: " + str(obs_environment.start) +  " End: " + str(obs_environment.end))
                 pv_obs_meta     = obs_environment.get_dwd_pv_data  (lat=latitude, lon=longitude, distance=distance, min_quality_per_parameter=min_quality_per_parameter)
