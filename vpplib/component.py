09ec1df53bd311663b641d6f2df347a9cc8c3218
Pyosch <birk-sascha@posteo.de>
2025-01-16 09:40:43 +0100

    removed cost from component

--- vpplib/component.py
+++ vpplib/component.py
@@ -13,7 +13,6 @@ class Component(object):
     def __init__(self,
                  unit=None,
                  environment=None,
-                 cost=None,
                  identifier=None):
         """
 
@@ -50,7 +49,6 @@ class Component(object):
         self.unit = unit  # e.g. "kW"
         self.identifier = identifier
         self.environment = environment
-        self.cost = cost  # e.g. Euro/kWh
 
     def value_for_timestamp(self, timestamp):
         """
